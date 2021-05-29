fcf4a362a0d241a519d5a00467ce9958003a3100
Billy Hodgson <billyhodgson@hotmail.com>
2021-05-28 20:56:24 -0600

    Creacion Tabla Vehiculo y Foreign Key

--- BaseDatos/dbo/Tables/MarcaVehiculo.sql
+++ BaseDatos/dbo/Tables/MarcaVehiculo.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[MarcaVehiculo]
 (
+
+	  MarcaVehiculoId INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_MArca_Vehiculo PRIMARY KEY  CLUSTERED (MarcaVehiculoId)
+	, Descripcion VARCHAR(250) NOT NULL 
+	, Estado BIT NOT NULL
 	  
-)
+) WITH (DATA_COMPRESSION = PAGE)
+GO
+
